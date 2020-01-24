2aad16ae1843f7fc93cbe73935752fbbbc085b55
AnthoTinette <59771340+AnthoTinette@users.noreply.github.com>
2020-01-24 09:35:22 +0100

    component creation

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/9e/73/bowl_delicious_dinner_dish_food_healthy_meal_tasty-1575227.jpg!d'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/9e/73/bowl_delicious_dinner_dish_food_healthy_meal_tasty-1575227.jpg!d')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
